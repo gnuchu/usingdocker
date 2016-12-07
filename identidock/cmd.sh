22639b9b4692704ea553c774154e6be0a61fa2c4
John Griffiths <gnuchu@gmail.com>
2016-12-07 21:46:12 +0000

    reorg

---
+++ identidock/cmd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$ENV" == 'DEV' ]; then
+	echo "Running Development server"
+	exec python "app/identidock.py"
+else
+	echo "Running Production server"
+	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191 --enable-threads
+fi
