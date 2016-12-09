cc9ce870ec95a818e0e9a65ecaa1575567b4b3e1
John Griffiths <gnuchu@gmail.com>
2016-12-09 11:59:54 +0000

    repo update for CI build

--- identidock/cmd.sh
+++ identidock/cmd.sh
@@ -5,6 +5,9 @@ set -e
 if [ "$ENV" == 'DEV' ]; then
 	echo "Running Development server"
 	exec python "identidock.py"
+elif [ "$ENV" == "UNIT" ]; then
+  echo "Running Unit Tests"
+  exec python "app/tests.py"
 else
 	echo "Running Production server"
 	exec uwsgi --http 0.0.0.0:9090 --wsgi-file /app/identidock.py --callable app --stats 0.0.0.0:9191 --enable-threads
